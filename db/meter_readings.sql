7411d43642619132b7811c8ab8cc55d8db0b0fd7
JorgeAJT <jorgeajtrueba@gmail.com>
2024-08-27 12:32:28 +0200

    fixed queries of create tables

--- db/meter_readings.sql
+++ db/meter_readings.sql
@@ -1,8 +1,8 @@
 CREATE TABLE meter_readings
 (
     meter_readings_id int GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    meter_number varchar(200) UNIQUE,
-    connection_ean_code varchar(200) UNIQUE,
+    meter_number varchar(200),
+    connection_ean_code varchar(200),
     account_id varchar(200),
     brand brand_enum,
     energy_type utility_type_enum,
