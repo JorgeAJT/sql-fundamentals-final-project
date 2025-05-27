18a53d2d4bcc4cb5dcd3dc7408a034323fb9b41d
JorgeAJT <jorgeajtrueba@gmail.com>
2025-05-27 16:29:43 +0200

    changed app to accept different path with env variables to work with different data in local or in the container

--- src/app.py
+++ src/app.py
@@ -1,16 +1,18 @@
+import os
 import logging
 from src.utils import database_connection, insert_data_from_json, fetch_data_as_json
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
+DATA_PATH = os.getenv("DATA_PATH", "./data")
 
 def run_database_insertions():
     try:
         conn = database_connection()
 
-        insert_data_from_json('./data/meter_data.json', conn, 'meter_data')
-        insert_data_from_json('./data/meter_readings.json', conn, 'meter_readings')
-        insert_data_from_json('./data/mandate_data.json', conn, 'mandate_data')
+        insert_data_from_json(f"{DATA_PATH}/meter_data.json", conn, "meter_data")
+        insert_data_from_json(f"{DATA_PATH}/meter_readings.json", conn, "meter_readings")
+        insert_data_from_json(f"{DATA_PATH}/mandate_data.json", conn, "mandate_data")
 
         fetch_data_as_json(conn, "SELECT * FROM mandate_data WHERE mandate_status = 'N'", 'selected_data.json')
 
